9b84ada457e605165fb6cc48e615c05b96d7ad44
Mikebird2414 <Mikebird2414@yahoo.com>
2021-04-16 18:18:11 -0400

    Package index

--- index.js
+++ index.js
@@ -1,107 +1,88 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
-const generateMd = require("./Develop/utils/generateMarkdown");
+const fs = require('fs');
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
 
-// set the fs.writeFile function to use promises
-const writeFileAsync = util.promisify(fs.writeFile);
-
-// array of questions to use with inquirer 
 const questions = [
     {
         type: "input",
-        message: "What is your GitHub user name?",
-        name: "username"
+        name: "title",
+        message: "What is your project title?"
     },
     {
         type: "input",
-        message: "What is your email address?",
-        name: "email"
+        name: "badge",
+        message: "Please provide the badges links that you want"
     },
     {
         type: "input",
-        message: "What is your project's title?",
-        name: "title"
+        name: "description",
+        message: "Please provide your project's description"
     },
     {
         type: "input",
-        message: "Please write a short description of your project.",
-        name: "description"
-    },
-    {
-        type: "list",
-        message: "What license should your project have?",
-        name: "license",
-        choices: [
-            "MIT",
-            "Unlicense",
-            "Apache 2.0",
-            "GNU v3",
-            "BSD 3-Clause",
-            "Mozilla Public License 2.0"
-        ]
-    },
-    {
-        type: "input",
-        message: "What command should be run to install dependencies?",
         name: "installation",
-        default: "npm i"
+        message: "Please provide the installation instructions"
     },
     {
         type: "input",
-        message: "What command should be run to run tests?",
-        name: "tests",
-        default: "npm run test"
+        name: "usage",
+        message: "Please provide the project usage"
     },
     {
         type: "input",
-        message: "What does the user need to know about using the repository?",
-        name: "usage"
+        name: "licence",
+        message: "Please provide the project licence or your badge link"
     },
     {
         type: "input",
-        message: "What does the user need to know about contributing to the repository?",
-        name: "contribute"
+        name: "contributing",
+        message: "Please provide the contributing parties"
     },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+];
 
-]
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
 
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file created with success!");
+          });
+        });
 
-// function to prompt user - returns answers object
-const promptUser = () => {
-    return inquirer
-        .prompt(questions);
+});
+
+function init() {
+
 }
 
-
-// function to write README file
-const writeToFile = (fileName, data) => {
-    return writeFileAsync(fileName, data);
-}
-
-
-// function to initialize program
-const init = async () => {
-    try {
-        console.log("Welcome to the README generator.\nPlease answer the following questions:")
-
-        // ask user for answers to questions
-        const answers = await promptUser();
-
-        // create markdown content from user answers
-        const fileContent = generateMd(answers);
-
-        // write markdown content to README.md file
-        await writeToFile("./output/README.md", fileContent);
-
-        // notify user that file has been written
-        console.log("README.md created in output folder.");
-
-    } catch (err) {
-        console.error("Error creating README. File not created.");
-        console.log(err);
-    }
-}
-
-// function call to initialize program
 init();
